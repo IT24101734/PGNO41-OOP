f68ae5c6972e8887cb96f813f4642701bb67c471
nethulinatharadhanapala <it24101574@my.sliit.lk>
2025-05-17 19:14:28 +0530

    Created the intial setup for Transaction class with required feilds.

--- src/main/java/com/movierental/model/rental/Transaction.java
+++ src/main/java/com/movierental/model/rental/Transaction.java
@@ -1,4 +1,20 @@
 package com.movierental.model.rental;
 
+import java.util.Date;
+
 public class Transaction {
+    private String transactionId;
+    private String userId;
+    private String movieId;
+    private Date rentalDate;
+    private Date dueDate;
+    private Date returnDate;
+    private double rentalFee;
+    private double lateFee;
+    private boolean returned;
+    private boolean canceled;
+    private String cancellationReason;
+    private Date cancellationDate;
+
+
 }
