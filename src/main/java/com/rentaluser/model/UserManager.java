dcbab2207edd6bac3ef2fc6923da4694cf72faf6
DELL <it24101590@my.sliit.lk>
2025-05-18 16:39:44 +0530

    UserManager Class for Managing User Data

--- src/main/java/com/rentaluser/model/UserManager.java
+++ src/main/java/com/rentaluser/model/UserManager.java
@@ -1,4 +1,22 @@
 package com.rentaluser.model;
 
 public class UserManager {
-}
+    public class UserManager {
+        private static final String USER_FILE_NAME = "users.txt";
+        private List<User> users;
+        private ServletContext servletContext;
+        private String dataFilePath;
+
+        // Constructor
+        public UserManager() {
+            this(null);
+        }
+
+        // Constructor with ServletContext
+        public UserManager(ServletContext servletContext) {
+            this.servletContext = servletContext;
+            users = new ArrayList<>();
+            initializeFilePath();
+            loadUsers();
+
+
