0db6062feddb43e95a462a2c9bde4257bab56c3b
Senuthi <it24101734@my.sliit.lk>
2025-05-18 16:42:40 +0530

    Using Getters and Setters in a Movie Class

--- src/main/java/com/movierental/model/movie/Movie.java
+++ src/main/java/com/movierental/model/movie/Movie.java
@@ -1,17 +1,61 @@
 package com.movierental.model.movie;
 
 public class Movie {
-    // Default constructor
-    public Movie() {
-        this.movieId = "";
-        this.title = "";
-        this.director = "";
-        this.genre = "";
-        this.releaseYear = 0;
-        this.rating = 0.0;
-        this.available = true;
-        this.coverPhotoPath = "";
-    }
-}
 
+    // Getters and setters (existing ones unchanged)
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
 
