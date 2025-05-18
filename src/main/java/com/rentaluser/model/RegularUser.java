c4967e22ddb6e16018a6a98e3f89235160f9682e
DELL <it24101590@my.sliit.lk>
2025-05-18 14:50:30 +0530

    Created the constructors in RegularUser.java

---
+++ src/main/java/com/rentaluser/model/RegularUser.java
@@ -0,0 +1,13 @@
+package com.rentaluser.model;
+
+public class RegularUser {
+    public RegularUser(String userId, String username, String password, String email, String fullName) {
+        super(userId, username, password, email, fullName);
+    }
+
+    // Default constructor
+    public RegularUser() {
+        super();
+    }
+
+}
