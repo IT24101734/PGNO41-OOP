2f1a9419c0436f34a9112b9bbff1cbe147f54a70
Chathuri <IT24200536@my.sliit.lk>
2025-05-18 15:10:06 +0530

    Recently Watched Stack Implementation

--- src/main/java/com/watchlist/model/RecentlyWatched.java
+++ src/main/java/com/watchlist/model/RecentlyWatched.java
@@ -1,4 +1,25 @@
 package com.watchlist.model;
 
+import java.util.Date;
+import java.util.Stack;
+
 public class RecentlyWatched {
+    private Stack<String> movieIds;     // Stack for movie IDs
+    private Stack<Date> watchDates;     // Parallel stack for watch dates
+    private int maxSize;                // maximum movies to keep track of
+
+    // Constructor
+    public RecentlyWatched(String userId) {
+        this.movieIds = new Stack<>();
+        this.watchDates = new Stack<>();
+        this.maxSize = 10;  // default to track last 10 movies
+    }
+
+    // Constructor with customizable size
+    public RecentlyWatched(String userId, int maxSize) {
+        this.movieIds = new Stack<>();
+        this.watchDates = new Stack<>();
+        this.maxSize = maxSize;
+    }
 }
+
