cf0adf55235c8e5bdfc13b9c29679770d38e8c82
DELL <it24101590@my.sliit.lk>
2025-05-18 16:43:58 +0530

    Initializing File Path for User Data Storage

--- src/main/java/com/rentaluser/model/UserManager.java
+++ src/main/java/com/rentaluser/model/UserManager.java
@@ -1,5 +1,7 @@
 package com.rentaluser.model;
 
+import java.io.File;
+
 public class UserManager {
     public class UserManager {
         private static final String USER_FILE_NAME = "users.txt";
@@ -18,5 +20,35 @@ public class UserManager {
             users = new ArrayList<>();
             initializeFilePath();
             loadUsers();
+
+        }
+
+        private void initializeFilePath() {
+            if (servletContext != null) {
+                // Use WEB-INF/data within the application context
+                String webInfDataPath = "/WEB-INF/data";
+                dataFilePath = servletContext.getRealPath(webInfDataPath) + File.separator + USER_FILE_NAME;
+
+                // Make sure directory exists
+                File dataDir = new File(servletContext.getRealPath(webInfDataPath));
+                if (!dataDir.exists()) {
+                    boolean created = dataDir.mkdirs();
+                    System.out.println("Created WEB-INF/data directory: " + dataDir.getAbsolutePath() + " - Success: " + created);
+                }
+            } else {
+                // Fallback to simple data directory if not in web context
+                String dataPath = "data";
+                dataFilePath = dataPath + File.separator + USER_FILE_NAME;
+
+                // Make sure directory exists
+                File dataDir = new File(dataPath);
+                if (!dataDir.exists()) {
+                    boolean created = dataDir.mkdirs();
+                    System.out.println("Created fallback data directory: " + dataPath + " - Success: " + created);
+                }
+            }
+
+            System.out.println("UserManager: Using data file path: " + dataFilePath);
+        }
 
 
