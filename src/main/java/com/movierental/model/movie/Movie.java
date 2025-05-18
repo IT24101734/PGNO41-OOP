fdde13164226bd7dc7142e030d21e9ecf4bd95e9
Senuthi <it24101734@my.sliit.lk>
2025-05-18 15:42:28 +0530

    Created basic Movie class

--- src/main/java/com/movierental/model/movie/Movie.java
+++ src/main/java/com/movierental/model/movie/Movie.java
@@ -1,4 +1,13 @@
 package com.movierental.model.movie;
 
 public class Movie {
+
+    private String movieId;
+    private String title;
+    private String director;
+    private String genre;
+    private int releaseYear;
+    private double rating;
+    private boolean available;
+    private String coverPhotoPath;
 }
