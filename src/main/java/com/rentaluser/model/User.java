f259b5fb7ee932e1996fe71f461dc3ec21e43ff6
DELL <it24101590@my.sliit.lk>
2025-05-18 15:23:25 +0530

    Basic User Attributes in User.java

---
+++ src/main/java/com/rentaluser/model/User.java
@@ -0,0 +1,10 @@
+package com.rentaluser.model;
+
+public class User {
+    private String userId;
+    private String username;
+    private String password;
+    private String email;
+    private String fullName;
+
+}
