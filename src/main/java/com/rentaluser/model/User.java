6ddb19112cbec26beb437783669253fd972bbfe0
DELL <it24101590@my.sliit.lk>
2025-05-18 12:31:27 +0530

    Initialized the attribute for the user class in constructors.

--- src/main/java/com/rentaluser/model/User.java
+++ src/main/java/com/rentaluser/model/User.java
@@ -1,4 +1,28 @@
 package com.rentaluser.model;
 
 public class User {
+    private String userId;
+    private String username;
+    private String password;
+    private String email;
+    private String fullName;
+
+    // Constructor with all fields
+    public User(String userId, String username, String password, String email, String fullName) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.fullName = fullName;
+    }
+
+    // Default constructor
+    public User() {
+        this.userId = "";
+        this.username = "";
+        this.password = "";
+        this.email = "";
+        this.fullName = "";
+    }
+
 }
