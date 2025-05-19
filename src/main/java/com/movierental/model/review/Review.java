c14229561efaaa9b0b634a34f14ec84940072923
Kaveesha <it24101571@my.sliit.lk>
2025-05-19 12:00:18 +0530

    Add getters,setters and toString() to Review

--- src/main/java/com/movierental/model/review/Review.java
+++ src/main/java/com/movierental/model/review/Review.java
@@ -11,17 +11,6 @@ public class Review {
     private int rating;
     private Date reviewDate;
 
-}
-
-public class Review {
-    private String reviewId;
-    private String movieId;
-    private String userId;
-    private String userName;
-    private String comment;
-    private int rating;
-    private Date reviewDate;
-
     public Review(String reviewId, String movieId, String userId, String userName, String comment, int rating, Date reviewDate) {
         this.reviewId = reviewId;
         this.movieId = movieId;
@@ -41,4 +30,69 @@ public class Review {
         this.rating = 0;
         this.reviewDate = new Date();
     }
+    public String getReviewId() {
+        return this.reviewId;
+    }
+
+    public void setReviewId(String reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getMovieId() {
+        return this.movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getReviewDate() {
+        return this.reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public boolean isVerified() {
+        return false;
+    }
+
+    public String toString() {
+        String var10000 = this.reviewId;
+        return "Review{reviewId='" + var10000 + "', movieId='" + this.movieId + "', userId='" + this.userId + "', userName='" + this.userName + "', comment='" + this.comment + "', rating=" + this.rating + ", reviewDate=" + String.valueOf(this.reviewDate) + "}";
+    }
+}
 
