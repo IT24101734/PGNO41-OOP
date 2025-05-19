6dfde4309d174cd6b3acafc63342d52758077b72
Kaveesha <it24101571@my.sliit.lk>
2025-05-19 12:14:44 +0530

    Add constructor and verified field to VerifiedReview

--- src/main/java/com/movierental/model/review/VerifiedReview.java
+++ src/main/java/com/movierental/model/review/VerifiedReview.java
@@ -6,3 +6,22 @@ import java.util.Date;
         private String transactionId;
         private Date watchDate;
 }
+
+    public VerifiedReview(String reviewId, String movieId, String userId, String userName, String comment, int rating, Date reviewDate, String transactionId, Date watchDate) {
+        super(reviewId, movieId, userId, userName, comment, rating, reviewDate);
+        this.transactionId = transactionId;
+        this.watchDate = watchDate;
+    }
+
+    public VerifiedReview(Review review, String transactionId, Date watchDate) {
+        super(review.getReviewId(), review.getMovieId(), review.getUserId(), review.getUserName(), review.getComment(), review.getRating(), review.getReviewDate());
+        this.transactionId = transactionId;
+        this.watchDate = watchDate;
+    }
+
+    public VerifiedReview() {
+        this.transactionId = "";
+        this.watchDate = new Date();
+    }
+
+
