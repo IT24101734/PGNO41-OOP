93d66c5b19c481cfd78c09588f57e5a5c8371c78
Chathuri <IT24200536@my.sliit.lk>
2025-05-19 12:54:05 +0530

    constructors

--- src/main/java/com/watchlist/model/WatchlistManager.java
+++ src/main/java/com/watchlist/model/WatchlistManager.java
@@ -1,4 +1,29 @@
 package com.watchlist.model;
 
 public class WatchlistManager {
-}
+
+    public class WatchlistManager {
+        private static final String WATCHLIST_FILE_NAME = "watchlists.txt";
+        private static final String RECENTLY_WATCHED_FILE_NAME = "recently_watched.txt";
+
+        private String watchlistFilePath;
+        private String recentlyWatchedFilePath;
+        private List<Watchlist> watchlists;
+        private Map<String, RecentlyWatched> recentlyWatchedMap;
+        private ServletContext servletContext;
+
+        // Default constructor
+        public WatchlistManager() {
+            this(null);
+        }
+
+        // Constructor with ServletContext
+        public WatchlistManager(ServletContext servletContext) {
+            this.servletContext = servletContext;
+            watchlists = new ArrayList<>();
+            recentlyWatchedMap = new HashMap<>();
+            initializeFilePaths();
+            loadWatchlists();
+            loadRecentlyWatched();
+        }
+
