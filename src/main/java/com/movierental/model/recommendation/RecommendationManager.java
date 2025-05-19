b0829de404ac1dc3f3cc26b219a5b352d96757d3
Anuththara <it24101784@my.sliit.lk>
2025-05-19 19:04:50 +0530

    created RecommendationManager class extending Recommendation with constructors

--- src/main/java/com/movierental/model/recommendation/RecommendationManager.java
+++ src/main/java/com/movierental/model/recommendation/RecommendationManager.java
@@ -1,4 +1,35 @@
 package com.movierental.model.recommendation;
 
+
+
 public class RecommendationManager {
+    private static final String RECOMMENDATION_FILE_NAME = "recommendations.txt";
+    private List<Recommendation> recommendations;
+    private MovieManager movieManager;
+    private ReviewManager reviewManager;
+    private RentalManager rentalManager;
+    private WatchlistManager watchlistManager;
+    private ServletContext servletContext;
+    private String dataFilePath;
+
+    // Constructor
+    public RecommendationManager() {
+        this(null);
+    }
+
+    // Constructor with ServletContext
+    public RecommendationManager(ServletContext servletContext) {
+        this.servletContext = servletContext;
+        recommendations = new ArrayList<>();
+        initializeFilePath();
+        loadRecommendations();
+
+        // Initialize other managers with the same ServletContext
+        movieManager = new MovieManager(servletContext);
+        reviewManager = new ReviewManager(servletContext);
+        rentalManager = new RentalManager(servletContext);
+        watchlistManager = new WatchlistManager(servletContext);
+    }
+
+
 }
