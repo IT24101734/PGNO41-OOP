614c22e94c785755b0e7e4b373bcc7de40db8f7e
Senuthi <it24101734@my.sliit.lk>
2025-05-19 12:38:10 +0530

    override method  in  Movie Class

--- src/main/java/com/movierental/model/movie/Movie.java
+++ src/main/java/com/movierental/model/movie/Movie.java
@@ -1,25 +1,18 @@
 package com.movierental.model.movie;
 
 public class Movie {
-
-    // New getter and setter for coverPhotoPath
-    public String getCoverPhotoPath() {
-        return coverPhotoPath;
-    }
-
-    public void setCoverPhotoPath(String coverPhotoPath) {
-        this.coverPhotoPath = coverPhotoPath;
-    }
-
-    // Calculate rental price (to be overridden by subclasses)
-    public double calculateRentalPrice(int daysRented) {
-        return 3.99 * daysRented; // Base rental price
-    }
-
-    // Update toFileString to include cover photo path
-    public String toFileString() {
-        return "REGULAR," + movieId + "," + title + "," + director + "," +
-                genre + "," + releaseYear + "," + rating + "," + available + "," + coverPhotoPath;
-
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieId='" + movieId + '\'' +
+                ", title='" + title + '\'' +
+                ", director='" + director + '\'' +
+                ", genre='" + genre + '\'' +
+                ", releaseYear=" + releaseYear +
+                ", rating=" + rating +
+                ", available=" + available +
+                ", coverPhotoPath='" + coverPhotoPath + '\'' +
+                '}';
     }
 }
+
