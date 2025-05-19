6e5bbb11e11d9ef37a7cbd69c5a3fe7faa6eb530
Kaveesha <it24101571@my.sliit.lk>
2025-05-19 13:05:44 +0530

    Setting Up File Path for Storing Reviews

--- src/main/java/com/movierental/model/review/ReviewManager.java
+++ src/main/java/com/movierental/model/review/ReviewManager.java
@@ -1,5 +1,7 @@
 package com.movierental.model.review;
 
+import java.io.File;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,30 @@ import java.util.List;
             this.reviews = new ArrayList();
             this.initializeFilePath();
             this.loadReviews();
+        }
+        private void initializeFilePath() {
+            if (this.servletContext != null) {
+                String webInfDataPath = "/WEB-INF/data";
+                String var10001 = this.servletContext.getRealPath(webInfDataPath);
+                this.dataFilePath = var10001 + File.separator + "reviews.txt";
+                File dataDir = new File(this.servletContext.getRealPath(webInfDataPath));
+                if (!dataDir.exists()) {
+                    boolean created = dataDir.mkdirs();
+                    PrintStream var10000 = System.out;
+                    var10001 = dataDir.getAbsolutePath();
+                    var10000.println("Created WEB-INF/data directory: " + var10001 + " - Success: " + created);
+                }
+            } else {
+                String dataPath = "data";
+                this.dataFilePath = dataPath + File.separator + "reviews.txt";
+                File dataDir = new File(dataPath);
+                if (!dataDir.exists()) {
+                    boolean created = dataDir.mkdirs();
+                    System.out.println("Created fallback data directory: " + dataPath + " - Success: " + created);
+                }
+            }
+
+            System.out.println("ReviewManager: Using data file path: " + this.dataFilePath);
         }
 
 
