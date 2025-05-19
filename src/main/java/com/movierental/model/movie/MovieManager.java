18133a4b18ab8f2c8024644f96a281eedc31c2e4
Senuthi <it24101734@my.sliit.lk>
2025-05-19 13:17:37 +0530

    MovieManager Path Setup

--- src/main/java/com/movierental/model/movie/MovieManager.java
+++ src/main/java/com/movierental/model/movie/MovieManager.java
@@ -1,4 +1,16 @@
 package com.movierental.model.movie;
 
+import java.util.List;
+
 public class MovieManager {
-}
+    public class MovieManager {
+        // Changed from a hardcoded path to a dynamic one
+        private static final String MOVIE_FILE_NAME = "movies.txt";
+        private static final String MOVIE_IMAGES_DIR = "movie_images";
+
+        private List<Movie> movies;
+        private ServletContext servletContext;
+        private String dataFilePath;
+        private String imagesDirectoryPath;
+
+    }
