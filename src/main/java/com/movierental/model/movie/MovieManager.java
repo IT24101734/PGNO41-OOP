8b8d6dc60bfdf6009fc9956ce3af0b77e0da43dc
Senuthi <it24101734@my.sliit.lk>
2025-05-19 14:06:31 +0530

    get avaliable movie

--- src/main/java/com/movierental/model/movie/MovieManager.java
+++ src/main/java/com/movierental/model/movie/MovieManager.java
@@ -1,41 +1,16 @@
 package com.movierental.model.movie;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MovieManager{
 
+    public List<Movie> getAvailableMovies() {
+        List<Movie> results = new ArrayList<>();
 
-public boolean updateMovie(Movie updatedMovie, InputStream coverPhotoStream, String originalFileName) {
-    for (int i = 0; i < movies.size(); i++) {
-        if (movies.get(i).getMovieId().equals(updatedMovie.getMovieId())) {
-            // If a new cover photo is provided, save it and update the path
-            if (coverPhotoStream != null) {
-                String fileExtension = getFileExtension(originalFileName);
-                String photoFileName = updatedMovie.getMovieId() + fileExtension;
-                String photoPath = imagesDirectoryPath + File.separator + photoFileName;
+        for (Movie movie : movies) {
+            if (movie.isAvailable()) {
+                results.add(movie);
 
-                try {
-                    savePhoto(coverPhotoStream, photoPath);
-                    updatedMovie.setCoverPhotoPath(MOVIE_IMAGES_DIR + "/" + photoFileName);
-                } catch (IOException e) {
-                    System.err.println("Error updating cover photo: " + e.getMessage());
-                    // Continue updating the movie even if photo update fails
-                    // Keep the old photo path
-                    updatedMovie.setCoverPhotoPath(movies.get(i).getCoverPhotoPath());
-                }
-            } else {
-                // No new photo provided, keep the old one
-                updatedMovie.setCoverPhotoPath(movies.get(i).getCoverPhotoPath());
+
             }
-
-            movies.set(i, updatedMovie);
-            saveMovies();
-            return true;
-        }
-    }
-    return false;
-}
-
-
-
-
-
-}
