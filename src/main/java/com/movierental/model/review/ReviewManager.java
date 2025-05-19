4acc0739c455319e1f678c0fc41f40344e83e561
Kaveesha <it24101571@my.sliit.lk>
2025-05-19 12:53:56 +0530

    Overloading constructors (ReviewManager() and ReviewManager(ServletContext servletContext)),

--- src/main/java/com/movierental/model/review/ReviewManager.java
+++ src/main/java/com/movierental/model/review/ReviewManager.java
@@ -1,5 +1,24 @@
 package com.movierental.model.review;
 
-public class ReviewManager {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+    public class ReviewManager {
+        private static final String REVIEW_FILE_NAME = "reviews.txt";
+        private List<Review> reviews;
+        private ServletContext servletContext;
+        private String dataFilePath;
+
+        public ReviewManager() {
+            this((ServletContext)null);
+        }
+
+        public ReviewManager(ServletContext servletContext) {
+            this.servletContext = servletContext;
+            this.reviews = new ArrayList();
+            this.initializeFilePath();
+            this.loadReviews();
+        }
+
+
+    }
