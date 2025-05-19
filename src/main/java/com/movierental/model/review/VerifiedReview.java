ec7ad59fb1305a7c2c43e1b4a7cabc9568812156
Kaveesha <it24101571@my.sliit.lk>
2025-05-19 12:08:27 +0530

    Create VerifiedReview class extending Review

--- src/main/java/com/movierental/model/review/VerifiedReview.java
+++ src/main/java/com/movierental/model/review/VerifiedReview.java
@@ -1,4 +1,8 @@
 package com.movierental.model.review;
 
-public class VerifiedReview {
+import java.util.Date;
+
+    public class VerifiedReview extends Review {
+        private String transactionId;
+        private Date watchDate;
 }
