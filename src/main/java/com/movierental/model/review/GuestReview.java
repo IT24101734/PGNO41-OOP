c199e67ae8f19cc4504f1c64dd65c6e89acbf6f5
Kaveesha <it24101571@my.sliit.lk>
2025-05-19 12:38:20 +0530

    Create the constructor

--- src/main/java/com/movierental/model/review/GuestReview.java
+++ src/main/java/com/movierental/model/review/GuestReview.java
@@ -1,7 +1,26 @@
 package com.movierental.model.review;
 
+import java.util.Date;
+
 public class GuestReview extends Review {
         private String emailAddress;
         private String ipAddress;
+}
+public GuestReview(String reviewId, String movieId, String userName, String comment, int rating, Date reviewDate, String emailAddress, String ipAddress) {
+        super(reviewId, movieId, (String)null, userName, comment, rating, reviewDate);
+        this.emailAddress = emailAddress;
+        this.ipAddress = ipAddress;
+}
+
+public GuestReview(Review review, String emailAddress, String ipAddress) {
+        super(review.getReviewId(), review.getMovieId(), (String)null, review.getUserName(), review.getComment(), review.getRating(), review.getReviewDate());
+        this.emailAddress = emailAddress;
+        this.ipAddress = ipAddress;
+}
+
+public GuestReview() {
+        this.setUserId((String)null);
+        this.emailAddress = "";
+        this.ipAddress = "";
 }
 
